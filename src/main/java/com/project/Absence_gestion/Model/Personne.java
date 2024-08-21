3f2d33c9c959a6a541af63c3022230b650306168
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-21 15:06:46 +0100

    creation de la strecture de projet

---
+++ src/main/java/com/project/Absence_gestion/Model/Personne.java
@@ -0,0 +1,33 @@
+package com.project.Absence_gestion.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="personne_type")
+public class Personne  {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String nom;
+
+    private String email;
+
+    private String password;
+}
+
+//    @Enumerated(EnumType.STRING)
+//    private Role role;
