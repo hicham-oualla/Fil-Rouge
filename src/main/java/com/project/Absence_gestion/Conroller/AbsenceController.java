3deb1f82a917d029bd9710d0eb8ed7c15351992d
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-25 15:35:19 +0100

    cretion de absencecontroller

---
+++ src/main/java/com/project/Absence_gestion/Conroller/AbsenceController.java
@@ -0,0 +1,57 @@
+package com.project.Absence_gestion.Conroller;
+import com.project.Absence_gestion.Model.Absence;
+import com.project.Absence_gestion.Service.AbsenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/absences")
+public class AbsenceController {
+
+    private final AbsenceService absenceService;
+
+    @Autowired
+    public AbsenceController(AbsenceService absenceService) {
+        this.absenceService = absenceService;
+    }
+
+
+    @PostMapping("/addAbsence")
+    public ResponseEntity<Absence> addAbsence(@RequestBody Absence absence) {
+        Absence createdAbsence = absenceService.addAbsence(absence);
+        return new ResponseEntity<>(createdAbsence, HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Absence> editAbsence(@PathVariable Long id, @RequestBody Absence absenceDetails) {
+        try {
+            Absence updatedAbsence = absenceService.editAbsence(id, absenceDetails);
+            return new ResponseEntity<>(updatedAbsence, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @GetMapping("/allabsence")
+    public ResponseEntity<List<Absence>> getAllAbsences() {
+        List<Absence> absences = absenceService.getAllAbsence();
+        return new ResponseEntity<>(absences, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAbsence(@PathVariable Long id) {
+        try {
+            absenceService.deleteAbsence(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
--- src/main/java/com/project/Absence_gestion/Model/Absence.java
+++ src/main/java/com/project/Absence_gestion/Model/Absence.java
@@ -1,16 +1,24 @@
 package com.project.Absence_gestion.Model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Absence {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Temporal(TemporalType.DATE)
-    private Date date;
+
+    private Date datedebut;
+
+    private Date datedefin;
 
     private int duration;
 
