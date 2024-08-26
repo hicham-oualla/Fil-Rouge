f2c7c47f33161408273e66c6ee9b20e956440cd7
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-27 00:05:32 +0100

    Creationde apprenantMapper

---
+++ src/main/java/com/project/Absence_gestion/mapper/ApprenantMapper.java
@@ -0,0 +1,18 @@
+package com.project.Absence_gestion.mapper;
+
+import com.project.Absence_gestion.dto.ApprenantDTO;
+import com.project.Absence_gestion.Model.Apprenant;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ApprenantMapper {
+    ApprenantMapper INSTANCE = Mappers.getMapper(ApprenantMapper.class);
+
+    @Mapping(source = "classe.id", target = "classeId")
+    ApprenantDTO toDTO(Apprenant apprenant);
+
+    @Mapping(source = "classeId", target = "classe.id")
+    Apprenant toEntity(ApprenantDTO apprenantDTO);
+}
