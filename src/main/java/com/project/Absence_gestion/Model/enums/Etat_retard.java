5b5272afcbec9ec940b00a5c7d86289700ac764e
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-26 17:52:04 +0100

    ajouter dependency de mapstruct

--- src/main/java/com/project/Absence_gestion/Model/enums/Etat_retard.java
+++ src/main/java/com/project/Absence_gestion/Model/enums/Etat_retard.java
@@ -1,5 +1,5 @@
 package com.project.Absence_gestion.Model.enums;
 
 public enum Etat_retard {
-    Justifier,nonjustifier
+    JUSITIFIER,NONJUSITIFIER
 }
--- src/main/java/com/project/Absence_gestion/Model/enums/Role.java
+++ src/main/java/com/project/Absence_gestion/Model/enums/Role.java
@@ -1,5 +1,5 @@
 package com.project.Absence_gestion.Model.enums;
 
 public enum Role {
-    Admin,apprenant
+    ADMIN,APPRENANT
 }
