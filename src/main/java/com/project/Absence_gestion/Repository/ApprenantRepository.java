2e46e53025b7232d404dde261cce18ea30351090
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-27 00:07:18 +0100

    add function  pour lister apprenant by class

--- src/main/java/com/project/Absence_gestion/Repository/ApprenantRepository.java
+++ src/main/java/com/project/Absence_gestion/Repository/ApprenantRepository.java
@@ -3,5 +3,8 @@ package com.project.Absence_gestion.Repository;
 import com.project.Absence_gestion.Model.Apprenant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ApprenantRepository extends JpaRepository<Apprenant, Integer> {
+import java.util.List;
+
+public interface ApprenantRepository extends JpaRepository<Apprenant, Long> {
+    List<Apprenant>GetAllApprenantByClasseId(Long classId);
 }
