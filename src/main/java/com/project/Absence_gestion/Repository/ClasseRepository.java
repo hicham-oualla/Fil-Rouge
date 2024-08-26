e3aa4288709034848780a52d0c89a9cff93edc68
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-26 15:25:36 +0100

    creation de ClasseRepository

---
+++ src/main/java/com/project/Absence_gestion/Repository/ClasseRepository.java
@@ -0,0 +1,7 @@
+package com.project.Absence_gestion.Repository;
+
+import com.project.Absence_gestion.Model.Classe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClasseRepository extends JpaRepository<Classe, Long> {
+}
