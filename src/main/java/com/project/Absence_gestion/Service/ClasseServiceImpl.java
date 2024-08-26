f09e3dd1f7b133988f8a526c4c63a8f4cdbb4b6d
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-26 15:51:31 +0100

    IPLEMNTATION  des methode de "class"

--- src/main/java/com/project/Absence_gestion/Service/ClasseServiceImpl.java
+++ src/main/java/com/project/Absence_gestion/Service/ClasseServiceImpl.java
@@ -1,4 +1,43 @@
 package com.project.Absence_gestion.Service;
 
-public class ClasseServiceImpl {
+import com.project.Absence_gestion.Model.Classe;
+import com.project.Absence_gestion.Repository.ClasseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ClasseServiceImpl implements ClasseService{
+    @Autowired
+    private ClasseRepository classeRepository;
+
+    @Override
+    public Classe saveClasse(Classe classe) {
+        return classeRepository.save(classe);
+    }
+
+    @Override
+    public List<Classe> getAllClasses() {
+        return classeRepository.findAll();
+    }
+
+    @Override
+    public Classe getClasseById(Long id) {
+        return classeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Classe updateClasse(Long id, Classe classe) {
+        if (classeRepository.existsById(id)) {
+            classe.setId(id);
+            return classeRepository.save(classe);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteClasse(Long id) {
+        classeRepository.deleteById(id);
+    }
+
 }
