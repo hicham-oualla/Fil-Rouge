9c44e1a75ed64a5f0d8447b95fa8fff36c4df261
hicham-oualla <hich2022am.oualla@gmail.com>
2024-09-09 10:07:05 +0100

    Update de controller avec dto

--- src/main/java/com/project/Absence_gestion/mapper/AbsenceMapper.java
+++ src/main/java/com/project/Absence_gestion/mapper/AbsenceMapper.java
@@ -3,10 +3,11 @@ import com.project.Absence_gestion.dto.AbsenceDTO;
 import com.project.Absence_gestion.Model.Absence;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AbsenceMapper {
 
     @Mapping(source = "apprenant.id", target = "apprenantId")
