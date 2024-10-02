9f9b5349749dffbc64cb1393731e049bf12136e6
hicham-oualla <hich2022am.oualla@gmail.com>
2024-10-02 14:17:10 +0100

    ajouet function de find by email

--- src/main/java/com/project/Absence_gestion/Repository/PerssoneRepository.java
+++ src/main/java/com/project/Absence_gestion/Repository/PerssoneRepository.java
@@ -3,5 +3,9 @@ package com.project.Absence_gestion.Repository;
 import com.project.Absence_gestion.Model.Personne;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PerssoneRepository extends JpaRepository<Personne, Long> {
+//    Optional<Personne> findByUsername(String nom);
+      Optional<Personne> findByEmail(String email);
 }
